25a7661e14a9e1afae4e1b8576a6f3a0c44c1ce5
Wyvern <110834832+Dowaren@users.noreply.github.com>
2023-10-08 03:28:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import wyvern
 import wyvern as  w
 import os
 import getpass
-
+import webbrowser
 w.wyvern()
 
 
@@ -12,7 +12,7 @@ def main():
         username = getpass.getuser()
         command = input(username + ": ")
 
-        if command == "help":
+        if command[0:4] == "help":
             print("""______________________________________________________________________
 Hi thanks for using Wyvern! 
                     
@@ -24,21 +24,36 @@ cls - clear the console
 
 cmd (command)-  (windows/linux/MacOS console command)
 
+web (url) - open link.
+
+git (https://github.com/{user}/{repo name}) - download git repo
 """)
 
 
 
-        if command == "cls":
+        elif command == "cls":
             os.system('cls')
             wyvern.wyvern()
 
         elif command[0:3] == "cmd":
+
+            print("""
+            """)
             os.system(command[4:])
+
             print(f'''
 Successfully executed command - "{command[4:]}"
             
             ''')
 
+
+        elif command[0:3] == "web":
+            webbrowser.open(command[4:], new=0, autoraise=True)
+
+
+        elif command[0:3] == "git":
+
+            webbrowser.open(command[4:] + "/archive/refs/heads/main.zip",  new=0, autoraise=True)
 
         else:
             print("Wywern: "
